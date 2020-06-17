41201e4b35700a2ee8ce943c00cc9cc1a4960ead
Karthikeyan Subramanian <skarthik.chennai@gmail.com>
2020-06-17 19:22:40 +0530

    Updates

---
+++ precache-manifest.3923d72129c9c334234c967808ad33fa.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7ff0cc263319546272fd949e5afe98af",
+    "url": "/Developer-Portfolio-Karthik/index.html"
+  },
+  {
+    "revision": "2a4b4cfbc2825f2a013b",
+    "url": "/Developer-Portfolio-Karthik/static/css/main.1c3ce961.chunk.css"
+  },
+  {
+    "revision": "d57f5a8dd0a07121ed05",
+    "url": "/Developer-Portfolio-Karthik/static/js/2.5ff30f50.chunk.js"
+  },
+  {
+    "revision": "2a4b4cfbc2825f2a013b",
+    "url": "/Developer-Portfolio-Karthik/static/js/main.3f78d30b.chunk.js"
+  },
+  {
+    "revision": "b55050c202dd0d9ddfe0",
+    "url": "/Developer-Portfolio-Karthik/static/js/runtime-main.3eab293c.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/Developer-Portfolio-Karthik/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "39a098def0f0dd5528e94997a8644b0e",
+    "url": "/Developer-Portfolio-Karthik/static/media/Guvi.39a098de.bin"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/Developer-Portfolio-Karthik/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "26ff4ceea17de0db3e9b53d262b316ad",
+    "url": "/Developer-Portfolio-Karthik/static/media/Wipro.26ff4cee.bin"
+  },
+  {
+    "revision": "d6d4086fb1cade58b3f786620e17c3ef",
+    "url": "/Developer-Portfolio-Karthik/static/media/contactMail.d6d4086f.png"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/Developer-Portfolio-Karthik/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/Developer-Portfolio-Karthik/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/Developer-Portfolio-Karthik/static/media/skill.34409398.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/Developer-Portfolio/precache-manifest.ad3c341e19043381967595a7488a8848.js"
+  "/Developer-Portfolio-Karthik/precache-manifest.3923d72129c9c334234c967808ad33fa.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Developer-Portfolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Developer-Portfolio-Karthik/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
